41f5461e68d6e377db2c5834321a378700bfe483
JustMrE <v.mr.who.v@gmail.com>
2023-03-04 00:41:18 +0600

    Временно отменил проверку открытия другим пользователем.

--- Program.cs
+++ Program.cs
@@ -18,22 +18,22 @@ namespace Meter
                 file = System.IO.Path.GetDirectoryName(dir) + @"\Счетчики.xlsm";
             }
             
-            string file1 = System.IO.Path.GetDirectoryName(dir) + @"\DB\opened.txt";
-            string username = Environment.UserName;
-            if (File.Exists(file1))
-            {
-                string newUserName = File.ReadAllText(file1);
-                if (!string.IsNullOrEmpty(newUserName))
-                {
-                    MessageBox.Show("Счетчики уже открыты пользователем " + newUserName);
-                    return;
-                }
-            }
-            using (StreamWriter writer = File.CreateText(file1))
-            {
-                writer.Write(username);
-                File.SetAttributes(file1, File.GetAttributes(file1) | FileAttributes.Hidden);
-            }
+            //string file1 = System.IO.Path.GetDirectoryName(dir) + @"\DB\opened.txt";
+            //string username = Environment.UserName;
+            //if (File.Exists(file1))
+            //{
+            //    string newUserName = File.ReadAllText(file1);
+            //    if (!string.IsNullOrEmpty(newUserName))
+            //    {
+            //        MessageBox.Show("Счетчики уже открыты пользователем " + newUserName);
+            //        return;
+            //    }
+            //}
+            //using (StreamWriter writer = File.CreateText(file1))
+            //{
+            //    writer.Write(username);
+            //    File.SetAttributes(file1, File.GetAttributes(file1) | FileAttributes.Hidden);
+            //}
 
             //// To customize application configuration such as set high DPI settings or default font,
             //// see https://aka.ms/applicationconfiguration.
@@ -43,7 +43,7 @@ namespace Meter
             Application.SetCompatibleTextRenderingDefault(false);
             MyApplicationContext myAppContext = new MyApplicationContext();
             Application.Run(myAppContext);
-            File.Delete(file1);
+            //File.Delete(file1);
         }    
     }
 }
